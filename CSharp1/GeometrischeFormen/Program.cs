24ea62d2d0828d3df5e68d276635bdaee6fc40b5
Serafino Fornito <fornito93@gmail.com>
2017-11-07 21:10:51 +0100

    feat(Aufgabe3.3): task "GeometrischeFormen" done

---
+++ CSharp1/GeometrischeFormen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometrischeFormen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrat quadrat = new Quadrat() { Seitenlaenge = 15 };
+            Kreis kreis = new Kreis() { Radius = 10 };
+            
+            Console.WriteLine("Quadratumfang: " + quadrat.Umfang.ToString());
+            Console.WriteLine("Quadratfläche: " + quadrat.BerechneFlaeche().ToString());
+            Console.WriteLine("Kreistumfang: " + kreis.Umfang.ToString());
+            Console.WriteLine("Kreisfläche: " + kreis.BerechneFlaeche().ToString());
+
+        }
+    }
+}
