554170ccc4f77fe1eec4647e22f1c81310fda34a
Serafino Fornito <fornito93@gmail.com>
2017-11-21 21:30:12 +0100

    feat(uebung 5.1): added new project

---
+++ CSharp1/Uebung_5-1/Kalkulation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung_5_1
+{
+    class Kalkulation
+    {
+        public float Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public float Add(float a, int b)
+        {
+            return a + b;
+        }
+
+        public float Add(float a, float b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ CSharp1/Uebung_5-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulation kalkulation = new Kalkulation();
+            Console.WriteLine(kalkulation.Add(5, 5));
+            Console.WriteLine(kalkulation.Add(5.0f, 5));
+            Console.WriteLine(kalkulation.Add(5.0f, 5.0f));
+        }
+    }
+}
