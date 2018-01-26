516ec45bd3e514cd49b8db595e596d8e60516dbb
Serafino Fornito <fornito93@gmail.com>
2018-01-26 19:14:03 +0100

    fix(Form1-saveAction): fixed save action feat(Form1-OnChange): added OnChange event logic for all the input fields in the details group

--- CSharp1/MLZ_RentalBoatManager/Form1.cs
+++ CSharp1/MLZ_RentalBoatManager/Form1.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace MLZ_RentalBoatManager
 {
     public partial class Form1 : Form
     {
+        [XmlArray("Boats")]
+        [XmlArrayItem("Boat", typeof(Boat))]
         public BindingList<Boat> BoatsList { get; set; }
+        public Boolean CancelEvent { get; set; }
 
         public Form1()
         {
 			InitializeComponent();
 
-			BoatsList = new BindingList<Boat>();
+            BoatsList = new BindingList<Boat>();
             categoryDropdown.DataSource = Enum.GetValues(typeof(Category));
             colorDropdown.DataSource = Enum.GetValues(typeof(Color));
         }
 
-		private void UpdateList()
+        private void UpdateList()
         {
             boatsFormList.DataSource = null;
             boatsFormList.DataSource = BoatsList;
@@ -27,17 +31,18 @@ namespace MLZ_RentalBoatManager
 
 		private void Form1_Load(object sender, EventArgs e)
         {
-			foreach (TextBox control in Controls.OfType<TextBox>())
-			{
-				Console.WriteLine(control.GetType());
-				//control.TextChanged += new System.EventHandler(OnContentChanged);
-			}
-			foreach (ComboBox control in Controls.OfType<ComboBox>())
-			{
-				control.SelectedIndexChanged += new System.EventHandler(OnContentChanged);
-			}
-
-			UpdateList();
+            foreach (GroupBox groupBox in detailGroupBox.Controls.OfType<GroupBox>())
+            {
+                foreach (TextBox control in groupBox.Controls.OfType<TextBox>())
+                {
+                    control.TextChanged += new System.EventHandler(OnContentChanged);
+                }
+                foreach (ComboBox control in groupBox.Controls.OfType<ComboBox>())
+                {
+                    control.TextChanged += new System.EventHandler(OnContentChanged);
+                }
+            }
+            UpdateList();
 		}
 
 		void BoatsList_ListChanged(object sender, ListChangedEventArgs e)
@@ -60,7 +65,8 @@ namespace MLZ_RentalBoatManager
 			int index = boatsFormList.SelectedIndex;
 			if (index >= 0)
 			{
-				imageBox.ImageLocation = BoatsList[index].ImagePath;
+                CancelEvent = true;
+                imageBox.ImageLocation = BoatsList[index].ImagePath;
 				brandInput.Text = BoatsList[index].Brand;
 				brandInput.Text = BoatsList[index].Brand;
 				modelInput.Text = BoatsList[index].Model;
@@ -76,7 +82,10 @@ namespace MLZ_RentalBoatManager
 				maxMotorSpeedInput.Text = BoatsList[index].MaxMotorSpeed.ToString();
 				maxSailSpeedInput.Text = BoatsList[index].MaxSailSpeed.ToString();
 			}
-		}
+
+            saveEntryBtn.Enabled = false;
+            CancelEvent = false;
+        }
 
 		private void saveEntryBtn_Click(object sender, EventArgs e)
 		{
@@ -86,46 +95,46 @@ namespace MLZ_RentalBoatManager
 				int index = boatsFormList.SelectedIndex;
 				if (index >= 0)
 				{
-					if (BoatsList[index].ImagePath == imageBox.ImageLocation)
+					if (BoatsList[index].ImagePath != imageBox.ImageLocation)
 						BoatsList[index].ImagePath = imageBox.ImageLocation;
 
-					if (BoatsList[index].Brand == brandInput.Text)
+					if (BoatsList[index].Brand != brandInput.Text)
 						BoatsList[index].Brand = brandInput.Text;
 
-					if (BoatsList[index].Model == modelInput.Text)
+					if (BoatsList[index].Model != modelInput.Text)
 						BoatsList[index].Model = modelInput.Text;
 
-					if (BoatsList[index].Category == (Category)categoryDropdown.SelectedIndex)
+					if (BoatsList[index].Category != (Category)categoryDropdown.SelectedIndex)
 						BoatsList[index].Category = (Category)categoryDropdown.SelectedIndex;
 
-					if (BoatsList[index].Color == (Color)colorDropdown.SelectedIndex)
+					if (BoatsList[index].Color != (Color)colorDropdown.SelectedIndex)
 						BoatsList[index].Color = (Color)colorDropdown.SelectedIndex;
 
-					if (BoatsList[index].Power == float.Parse(powerInput.Text))
+					if (BoatsList[index].Power != float.Parse(powerInput.Text))
 						BoatsList[index].Power = float.Parse(powerInput.Text);
 
-					if (BoatsList[index].LicensePlate == licensePlateInput.Text)
+					if (BoatsList[index].LicensePlate != licensePlateInput.Text)
 						BoatsList[index].LicensePlate = licensePlateInput.Text;
 
-					if (BoatsList[index].Length == float.Parse(lengthInput.Text))
+					if (BoatsList[index].Length != float.Parse(lengthInput.Text))
 						BoatsList[index].Length = float.Parse(lengthInput.Text);
 
-					if (BoatsList[index].Width == float.Parse(widthInput.Text))
+					if (BoatsList[index].Width != float.Parse(widthInput.Text))
 						BoatsList[index].Width = float.Parse(widthInput.Text);
 
-					if (BoatsList[index].Height == float.Parse(heightInput.Text))
+					if (BoatsList[index].Height != float.Parse(heightInput.Text))
 						BoatsList[index].Height = float.Parse(heightInput.Text);
 
-					if (BoatsList[index].RentPerDay == float.Parse(rentPerDayInput.Text))
+					if (BoatsList[index].RentPerDay != float.Parse(rentPerDayInput.Text))
 						BoatsList[index].RentPerDay = float.Parse(rentPerDayInput.Text);
 
-					if (BoatsList[index].NumberOfPerson == int.Parse(numberOfPersonInput.Text))
+					if (BoatsList[index].NumberOfPerson != int.Parse(numberOfPersonInput.Text))
 						BoatsList[index].NumberOfPerson = int.Parse(numberOfPersonInput.Text);
 
-					if (BoatsList[index].MaxMotorSpeed == int.Parse(maxMotorSpeedInput.Text))
+					if (BoatsList[index].MaxMotorSpeed != int.Parse(maxMotorSpeedInput.Text))
 						BoatsList[index].MaxMotorSpeed = int.Parse(maxMotorSpeedInput.Text);
 
-					if (BoatsList[index].MaxSailSpeed == int.Parse(maxSailSpeedInput.Text))
+					if (BoatsList[index].MaxSailSpeed != int.Parse(maxSailSpeedInput.Text))
 						BoatsList[index].MaxSailSpeed = int.Parse(maxSailSpeedInput.Text);
 
 					UpdateList();
@@ -182,7 +191,8 @@ namespace MLZ_RentalBoatManager
 
 		private void OnContentChanged(object sender, EventArgs e)
 		{
-			MessageBox.Show("OnContentChanged triggered");
-		}
+            if (CancelEvent) return;
+            saveEntryBtn.Enabled = true;
+        }
 	}
 }
