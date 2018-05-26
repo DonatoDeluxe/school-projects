a4b286071a3277ba5eac2b3ee66ee58b4d574638
Serafino Fornito <fornito93@gmail.com>
2018-05-26 13:51:48 +0200

    feat(new project): creted new project "Book_MVVM_Step_5"

---
+++ CSharp2/Book_MVVM_Step_5/Model/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Book_MVVM_Step_5.Model
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ CSharp2/Book_MVVM_Step_5/View/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Book_MVVM_Step_5.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
