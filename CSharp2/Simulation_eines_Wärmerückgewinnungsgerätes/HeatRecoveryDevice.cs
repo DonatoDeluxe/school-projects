7ee87b0d1bbe2243a3694ab2627f17066d8633d5
Serafino Fornito (DESKTOP) <fornito93@gmail.com>
2018-05-28 19:36:02 +0200

    fix(changed properies type): changed properties types from int to double feat(created level selection logic): created selection- & current-level changed logic feat(created level selection logic): created steps-logic to get to the selected level

--- CSharp2/Simulation_eines_Wärmerückgewinnungsgerätes/HeatRecoveryDevice.cs
+++ CSharp2/Simulation_eines_Wärmerückgewinnungsgerätes/HeatRecoveryDevice.cs
@@ -9,28 +9,40 @@ namespace Simulation_eines_Wärmerückgewinnungsgerätes
 {
     class HeatRecoveryDevice
     {
-        private static int[,] levelsStats = new int[4, 2] { {0, 0}, {30, 15}, {60, 45}, {90, 60} };
-        private static int[] currentSteps = new int[2] {0, 0};
-
-        private static Timer secTimer = new Timer(1000);
+        private static Timer secTimer = new Timer(500);
+        private static readonly double[,] levelsStats = new double[4, 2] { {0, 0}, {30, 15}, {60, 45}, {90, 60} };
 
         private int currentLevel;
-        public int CurrentLevel { get { return currentLevel; } private set {
+        public int CurrentLevel
+        {
+            get { return currentLevel; }
+            private set
+            {
+                if (currentLevel == value)
+                    return;
                 currentLevel = value;
-                ContentChanged();
-                //OnContentChanged();
-            } }
-        public int SelectedLevel { get; private set; }
-        public int EngineSpeed { get; private set; }
-        public int Current { get; private set; }
-        public int Voltage { get; private set; }
-        public int Power { get; private set; }
+                CurrentLevelChanged();
+            }
+        }
 
-        //public event EventHandler ContentChanged;
-        //public void OnContentChanged()
-        //{
-        //    ContentChanged?.Invoke(this, EventArgs.Empty);
-        //}
+        private int selectedLevel;
+        public int SelectedLevel
+        {
+            get { return selectedLevel; }
+            set
+            {
+                if (selectedLevel == value)
+                    return;
+                selectedLevel = value;
+                SelectedLevelChanged();
+            }
+        }
+
+        public double EngineSpeed { get; private set; }
+        public double Current { get; private set; }
+        public double Voltage { get; private set; }
+        public double Power { get; private set; }
+        public double[] CurrentSteps = new double[levelsStats.GetLength(1)];
 
         public HeatRecoveryDevice() {
             EngineSpeed = 0;
@@ -43,31 +55,98 @@ namespace Simulation_eines_Wärmerückgewinnungsgerätes
             secTimer.Elapsed += OnSecondElapsed;
         }
 
-        public void ContentChanged()
+        public override string ToString()
+        {
+            return String.Format($"EngineSpeed: {EngineSpeed}\tCurrent: {Current}\tVoltage: {Voltage}\tPower: {Power}\t");
+        }
+
+        public void CurrentLevelChanged()
+        {
+            SetStepsValues();
+        }
+
+        public void SelectedLevelChanged()
+        {
+            if (Voltage < 230 && SelectedLevel > 0)
+                Voltage = 230;
+            SetStepsValues();
+        }
+
+        private void SetStepsValues()
         {
             var diffLevels = SelectedLevel - CurrentLevel;
+
+            if (diffLevels == 0)
+            {
+                if(EngineSpeed == levelsStats[SelectedLevel, 0])
+                {
+                    secTimer.Enabled = false;
+                    Array.Clear(CurrentSteps, 0, CurrentSteps.Length);
+                    return;
+                }
+
+                if (EngineSpeed < levelsStats[SelectedLevel, 0])
+                    currentLevel = SelectedLevel - 1;
+                else
+                    currentLevel = SelectedLevel + 1;
+
+                diffLevels = SelectedLevel - CurrentLevel;
+            }
+
+            int timeToGoal = 5; //in seconds
+            var levelUpOrDown = diffLevels / Math.Abs(diffLevels);
+            var nextLevel = CurrentLevel + levelUpOrDown;
+
+            if (diffLevels < 0)
+                timeToGoal = ((CurrentLevel * CurrentLevel) - (nextLevel * nextLevel)) * 10;
+
+            Console.WriteLine("current level: " + currentLevel);
+            Console.WriteLine("selected level: " + selectedLevel);
+            Console.WriteLine("level up or down: " + levelUpOrDown);
+            Console.WriteLine("next level: " + nextLevel);
+            Console.WriteLine("steps(seconds) to goal: " + timeToGoal);
+
+            for (int i = 0; i < levelsStats.GetLength(1); i++)
+            {
+                CurrentSteps[i] = (levelsStats[nextLevel, i] - levelsStats[CurrentLevel, i]) / timeToGoal;
+            }
+
+            if (secTimer.Enabled != true)
+                secTimer.Enabled = true;
         }
 
         public void OnSecondElapsed(Object source, ElapsedEventArgs e)
         {
-            if(EngineSpeed != levelsStats[SelectedLevel, 0])
-            {
-                var diffLevels = SelectedLevel - CurrentLevel;
-                
-            }
-
-            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
-                              e.SignalTime);
+            DoDeviceStatsStep();
+            Console.WriteLine(this.ToString());
         }
 
-        public void ChangeSelectedLevel(int newSelectedLevel)
+        private void DoDeviceStatsStep()
         {
-            SelectedLevel = newSelectedLevel;
-            var diffLevels = SelectedLevel - CurrentLevel;
-            if (diffLevels != 0)
-            {
+            if (CurrentSteps[0] == 0)
+                return;
 
+            EngineSpeed += CurrentSteps[0];
+            Power += CurrentSteps[1];
+
+            var diffLevels = SelectedLevel - CurrentLevel;
+            var levelUpOrDown = diffLevels / Math.Abs(diffLevels);
+            var nextLevel = CurrentLevel + levelUpOrDown;
+
+            if ((CurrentSteps[0] < 0 && EngineSpeed <= levelsStats[nextLevel, 0]) || (CurrentSteps[0] > 0 && EngineSpeed >= levelsStats[nextLevel, 0]))
+            {
+                EngineSpeed = levelsStats[nextLevel, 0];
+                Power = levelsStats[nextLevel, 1];
+                CurrentLevel = nextLevel;
+
+                if (CurrentLevel + SelectedLevel == 0)
+                    Voltage = 0;
             }
+
+            if (Voltage != 0)
+                Current = Power / Voltage;
+            else
+                Current = 0;
         }
     }
 }
